7ed33dff9901a0ff04421fca4c76d9edfe0cb70f
nombumurage <nombumurage@gmail.com>
2017-04-25 16:03:14 +0300

    Tuesday, April 25th: Firebase

--- app/src/main/java/com/example/findus/findme/SignUpActivity.java
+++ app/src/main/java/com/example/findus/findme/SignUpActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
